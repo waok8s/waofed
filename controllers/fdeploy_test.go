f5149c78922e94b1deb0fce7353682d8344308b9
Shunsuke Ise <ise@bitmedia.co.jp>
2022-11-28 18:15:45 +0900

    test fdeploy more

--- controllers/fdeploy_test.go
+++ controllers/fdeploy_test.go
@@ -128,12 +128,30 @@ func Test_convertToStructuredFederatedDeployment(t *testing.T) {
 			},
 			false,
 		},
+		{"wrong GVK",
+			args{&unstructured.Unstructured{Object: helperLoadJSON(t, "testdata/unstructuredFederatedDeploymentObject_wrong_GVK.json")}},
+			&structuredFederatedDeployment{},
+			true,
+		},
+		{"no_meta",
+			args{&unstructured.Unstructured{Object: helperLoadJSON(t, "testdata/unstructuredFederatedDeploymentObject_no_meta.json")}},
+			&structuredFederatedDeployment{},
+			true,
+		},
+		{"no_spec",
+			args{&unstructured.Unstructured{Object: helperLoadJSON(t, "testdata/unstructuredFederatedDeploymentObject_no_spec.json")}},
+			&structuredFederatedDeployment{},
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := convertToStructuredFederatedDeployment(tt.args.in)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("convertToStructuredFederatedDeployment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil {
 				return
 			}
 			ok := true
@@ -179,4 +197,200 @@ func helperLoadJSON(t *testing.T, filename string) map[string]any {
 		t.Error(err)
 	}
 	return j
+}
+
+func Test_sameOwner(t *testing.T) {
+	type args struct {
+		a metav1.OwnerReference
+		b metav1.OwnerReference
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"same", args{
+			a: metav1.OwnerReference{
+				APIVersion:         "types.kubefed.io/v1beta1",
+				Kind:               "FederatedDeployment",
+				Name:               "fdeploy-sample",
+				UID:                "db5574ab-3fbf-44ce-b8f0-4f231d333f6e",
+				Controller:         pointer.Bool(true),
+				BlockOwnerDeletion: pointer.Bool(true),
+			},
+			b: metav1.OwnerReference{
+				APIVersion:         "types.kubefed.io/v1beta1",
+				Kind:               "FederatedDeployment",
+				Name:               "fdeploy-sample",
+				UID:                "d3ea102a-f80b-4d0e-90a3-115924cd97a2",
+				Controller:         nil,
+				BlockOwnerDeletion: nil,
+			},
+		}, true},
+		{"different", args{
+			a: metav1.OwnerReference{
+				APIVersion:         "types.kubefed.io/v1beta1",
+				Kind:               "FederatedDeployment",
+				Name:               "fdeploy-sample",
+				UID:                "db5574ab-3fbf-44ce-b8f0-4f231d333f6e",
+				Controller:         pointer.Bool(true),
+				BlockOwnerDeletion: pointer.Bool(true),
+			},
+			b: metav1.OwnerReference{
+				APIVersion:         "types.kubefed.io/v1beta1",
+				Kind:               "FederatedDeployment",
+				Name:               "fdeploy-sample2",
+				UID:                "db5574ab-3fbf-44ce-b8f0-4f231d333f6e",
+				Controller:         pointer.Bool(true),
+				BlockOwnerDeletion: pointer.Bool(true),
+			},
+		}, false},
+		{"invalid", args{
+			a: metav1.OwnerReference{
+				APIVersion:         "a/b/c",
+				Kind:               "d",
+				Name:               "e",
+				UID:                "6a57571c-9b4b-44a4-bd3a-58fe139c51ce",
+				Controller:         nil,
+				BlockOwnerDeletion: nil,
+			},
+			b: metav1.OwnerReference{
+				APIVersion:         "a/b/c",
+				Kind:               "d",
+				Name:               "e",
+				UID:                "6a57571c-9b4b-44a4-bd3a-58fe139c51ce",
+				Controller:         nil,
+				BlockOwnerDeletion: nil,
+			},
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sameOwner(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("sameOwner() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var (
+	fdep1 = structuredFederatedDeployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       federatedDeploymentGVK.Kind,
+			APIVersion: federatedDeploymentGVK.GroupVersion().Identifier(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fdeploy-sample",
+			Namespace: "default",
+		},
+		Spec: &structuredFederatedDeploymentSpec{
+			Placement: &util.GenericPlacementFields{
+				Clusters: []util.GenericClusterReference{
+					{Name: "kind-waofed-1"}, {Name: "kind-waofed-2"}, {Name: "kind-waofed-3"}},
+			},
+			Template: &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "",
+					Namespace: "",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Replicas: pointer.Int32(9),
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"": "",
+						},
+					},
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "nginx",
+									Image: "nginx:1.23.2",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+)
+
+func Test_setControllerReference(t *testing.T) {
+	type args struct {
+		owner      *structuredFederatedDeployment
+		controlled metav1.Object
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"no owners", args{
+			owner: &fdep1,
+			controlled: &metav1.ObjectMeta{
+				Name:            "hoge",
+				Namespace:       "default",
+				OwnerReferences: []metav1.OwnerReference{},
+			},
+		}, false},
+		{"owned by self", args{
+			owner: &fdep1,
+			controlled: &metav1.ObjectMeta{
+				Name:      "hoge",
+				Namespace: "default",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "types.kubefed.io/v1beta1",
+						Kind:               "FederatedDeployment",
+						Name:               "fdeploy-sample",
+						UID:                "",
+						Controller:         pointer.Bool(false),
+						BlockOwnerDeletion: pointer.Bool(false),
+					},
+				},
+			},
+		}, false},
+		{"controlled by self", args{
+			owner: &fdep1,
+			controlled: &metav1.ObjectMeta{
+				Name:      "hoge",
+				Namespace: "default",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "types.kubefed.io/v1beta1",
+						Kind:               "FederatedDeployment",
+						Name:               "fdeploy-sample",
+						UID:                "",
+						Controller:         pointer.Bool(true),
+						BlockOwnerDeletion: pointer.Bool(true),
+					},
+				},
+			},
+		}, false},
+		{"controlled by other resource", args{
+			owner: &fdep1,
+			controlled: &metav1.ObjectMeta{
+				Name:      "hoge",
+				Namespace: "default",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "types.kubefed.io/v1beta1",
+						Kind:               "FederatedDeployment",
+						Name:               "fdeploy-sample2",
+						UID:                "",
+						Controller:         pointer.Bool(true),
+						BlockOwnerDeletion: pointer.Bool(true),
+					},
+				},
+			},
+		}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := setControllerReference(tt.args.owner, tt.args.controlled); (err != nil) != tt.wantErr {
+				t.Errorf("setControllerReference() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
